bd50b1941eee246afb99ce61d385c1dd14fd6d4a
Albert Peci <albert.peci@posteo.de>
2022-06-14 13:40:29 +0200

    feat: add did creator and verify against compliance service

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const fs = require('fs').promises
 const jose = require('jose')
 
 const { selfDescription } = require('./self-description.json')
+const currentTime = new Date().getTime()
+const BASE_URL = "https://compliance.gaia-x.eu"
 
 async function canonize(selfDescription) {
-  const url = 'https://compliance.lab.gaia-x.eu/api/v1/normalize'
-  const { data } = await axios.post(url, { selfDescription })
+  const URL = BASE_URL + '/api/v1/normalize'
+  const { data } = await axios.post(URL, { selfDescription })
 
   return data
 }
@@ -32,7 +34,7 @@ async function sign(hash) {
 async function createProof(hash) {
   const proof = {
     type: 'JsonWebKey2020',
-    created: new Date().toISOString(),
+    created: new Date(currentTime).toISOString(),
     proofPurpose: 'assertionMethod',
     verificationMethod: process.env.VERIFICATION_METHOD ?? 'did:web:compliance.lab.gaia-x.eu',
     jws: await sign(hash)
@@ -44,8 +46,12 @@ async function createProof(hash) {
 async function verify(jws) {
   const algorithm = 'PS256'
   const x509 = await jose.importX509(process.env.PUBLIC_KEY, algorithm)
+  const publicKeyJwk = await jose.exportJWK(x509)
+
+  const pubkey = await jose.importJWK(publicKeyJwk, 'PS256')
+
   try {
-    const result = await jose.compactVerify(jws, x509)
+    const result = await jose.compactVerify(jws, pubkey)
 
     return { protectedHeader: result.protectedHeader, content: new TextDecoder().decode(result.payload) }
   } catch (error) {
@@ -53,10 +59,41 @@ async function verify(jws) {
   }
 }
 
-async function saveResultToFile(selfDescription, proof) {
-  const content = { selfDescription, proof }
+async function createSignedSdFile(selfDescription, proof, complianceCredential) {
+  const content = complianceCredential ?
+    { selfDescriptionCredential: { selfDescription, proof }, complianceCredential: complianceCredential }
+    : { selfDescription, proof }
+  const type = complianceCredential ? "complete" : "self-signed"
   const data = JSON.stringify(content, null, 2)
-  const filename = `${selfDescription['@type'].split(':')[0]}-${new Date().getTime()}.json`
+  const filename = `${currentTime}_${type}_${selfDescription['@type'].split(':')[0]}.json`
+
+  await fs.writeFile(filename, data)
+
+  return filename
+}
+
+async function createDIDFile() {
+  const algorithm = 'PS256'
+  const x509 = await jose.importX509(process.env.PUBLIC_KEY, algorithm)
+  const publicKeyJwk = await jose.exportJWK(x509)
+  publicKeyJwk.alg = algorithm
+
+  const did = {
+    "@context": ["https://www.w3.org/ns/did/v1"],
+    "id": process.env.VERIFICATION_METHOD,
+    "verificationMethod": [
+      {
+        "@context": "https://w3c-ccg.github.io/lds-jws2020/contexts/v1/",
+        "id": process.env.VERIFICATION_METHOD + "#JWK2020-RSA",
+        publicKeyJwk,
+        "x5u": process.env.X5U_URL
+      }
+    ],
+    "assertionMethod": [process.env.VERIFICATION_METHOD + "#JWK2020-RSA",]
+  }
+
+  const data = JSON.stringify(did, null, 2)
+  const filename = `${currentTime}_did.json`
 
   await fs.writeFile(filename, data)
 
@@ -67,9 +104,27 @@ function logger(...msg) {
   console.log(msg.join(" "))
 }
 
+async function signSd(selfDescription, proof) {
+  const URL = BASE_URL + "/api/v1/sign"
+  const { data } = await axios.post(URL, { selfDescription, proof })
+
+  return data
+}
+
+async function verifySelfDescription(selfDescription, proof, complianceCredential) {
+  const URL = BASE_URL + "/api/v1/participant/verify/raw"
+  try {
+  const { data } = await axios.post(URL, { selfDescriptionCredential: { selfDescription, proof }, complianceCredential: complianceCredential })
+
+  return data
+} catch (error) {
+  return {}
+}
+}
+
 async function main() {
   const canonizedSD = await canonize(selfDescription)
-  
+
   const hash = sha256(canonizedSD)
   logger(`📈 Hashed canonized SD ${hash}`)
 
@@ -77,10 +132,25 @@ async function main() {
   logger(`🔒 Proof created (${process.env.VERIFICATION_METHOD ?? 'did:web:compliance.lab.gaia-x.eu'})`)
 
   const verificationResult = await verify(proof.jws.replace('..', `.${hash}.`))
-  logger(verificationResult?.content === hash ? '✅ Verification successful' : '❌ Verification failed')
+  logger(verificationResult?.content === hash ? '✅ Verification successful (local)' : '❌ Verification failed (local)')
 
-  const filename = await saveResultToFile(selfDescription, proof)
-  logger(`📁 ${filename} saved`)
+  const filenameSignedSd = await createSignedSdFile(selfDescription, proof)
+  logger(`📁 ${filenameSignedSd} saved`)
+
+  const filenameDid = await createDIDFile()
+  logger(`📁 ${filenameDid} saved`)
+
+  const complianceCredential = await signSd(selfDescription, proof)
+  logger(complianceCredential ? '✅ SD signed successfully (compliance service)' : '❌ SD signing failed (compliance service)')
+
+  if (complianceCredential) {
+    const filenameCompleteSd = await createSignedSdFile(selfDescription, proof, complianceCredential)
+    logger(`📁 ${filenameCompleteSd} saved`)
+
+    const verificationResultRemote = await verifySelfDescription( selfDescription, proof, complianceCredential)
+    logger(verificationResultRemote?.conforms === true ? '✅ Verification successful (compliance service)' : '❌ Verification failed (compliance service)')
+
+  }
 }
 
 main()
